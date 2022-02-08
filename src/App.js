0575b15a3a1f9f6440524f9e40498a84e45416ce
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-02-08 11:33:46 -0600

    commit tree fab60515f283702bf5c93e0ccdd88bb4767bc412

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './App.css';
+import { addTodo } from  './actions/todos';
 
 class App extends Component {
 
   state = {
     todo: ''
+  }
+
+  addTodo = () => {
+    return ({
+      type: 'ADD_TODO',
+      todo: this.state.todo
+    })
   }
 
   handleOnChange = event => {
@@ -17,7 +25,7 @@ class App extends Component {
   handleOnSubmit = event => {
     event.preventDefault();
     console.log("Todo being added: ", this.state.todo);
-    this.props.dispatch({ type: 'ADD_TODO', todo: this.state.todo });
+    this.props.addTodo(this.state.todo);
     this.setState({ todo: '' });
   }
 
@@ -41,10 +49,4 @@ class App extends Component {
   }
 };
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos
-  };
-};
-
-export default connect(mapStateToProps)(App);
+export default connect(state => ({ todos: state.todos }), { addTodo })(App);
